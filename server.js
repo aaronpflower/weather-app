47fb17252cf774cf377d6769843e52bb698d438f
Aaron Flower <aaronpflower@gmail.com>
2017-04-20 15:01:52 -0600

    removed old dependinces

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
+const morgan = require('morgan');
 const helmet = require('helmet')
 const port = process.env.PORT || 3000
 const env = process.env.NODE_ENV || 'development'
@@ -13,6 +14,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.raw());
 app.use(bodyParser.text());
+
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/', function(request, response) {
