4fe3c7501defbd22f13bb6c4564ed53098660d82
Aaron Flower <aaronpflower@gmail.com>
2017-04-25 19:41:10 -0600

    fixed db locally

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,10 @@
-const databaseName = 'weatherapp';
-
+require("dotenv").config();
+// todo: 1 figure out how to run mocha tests on staging env?
 module.exports = {
+
     development: {
         client: 'postgresql',
-        connection: `postgres://localhost:5432/${databaseName}`,
+        connection: process.env.DATABASE_URL,
         migrations: {
             directory: __dirname + '/server/db/migrations'
         },
@@ -11,9 +12,10 @@ module.exports = {
             directory: __dirname + '/server/db/seeds'
         }
     },
+    
     test: {
         client: 'postgresql',
-        connection: `postgres://localhost:5432/${databaseName}_test`,
+        connection: process.env.DATABASE_URL_TEST,
         migrations: {
             directory: __dirname + '/server/db/migrations'
         },
@@ -21,14 +23,36 @@ module.exports = {
             directory: __dirname + '/server/db/seeds'
         }
     },
-    production: {
-        client: 'postgresql',
-        connection: process.env.DATABASE_URL + '?ssl=true',
-        migrations: {
-            directory: __dirname + '/server/db/migrations'
-        },
-        seeds: {
-            directory: __dirname + '/server/db/seeds'
-        }
-    }
+
+	staging: {
+		client: 'postgresql',
+		connection: process.env.DATABASE_URL + '?ssl=true',
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			tableName: 'knex_migrations'
+		}
+	},
+
+	production: {
+		client: 'postgresql',
+		connection: process.env.DATABASE_URL + '?ssl=true',
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			tableName: 'knex_migrations'
+		}
+	}
+
 };
+
+// connection: {
+		// 	database: 'my_db',
+		// 	user:     'username',
+		// 	password: 'password'
+		// },
+
--- server/db/connection.js
+++ server/db/connection.js
@@ -1,4 +1,4 @@
 const environment = process.env.NODE_ENV;
 const config = require('../../knexfile.js')[environment];
-
+console.log(config)
 module.exports = require('knex')(config);
